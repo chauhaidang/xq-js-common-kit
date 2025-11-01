31e02d5b2408b3cff8763b0e551e9df2b48a0d1c
chauhaidang1 <chauhaidang1@gmail.com>
2025-11-01 22:19:41 +0700

    fix missing export from markdown.js

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const { generateRandomString } = require('./src/string')
 const { getConfig } = require('./src/config')
 const { readYAML } = require('./src/yaml')
 const { logger } = require('./src/logger')
+const { generateMarkdownFromJunit } = require('./src/markdown')
+
 module.exports = {
     generateRandomString,
     getConfig,
     readYAML,
-    logger
+    logger,
+    generateMarkdownFromJunit
 }
